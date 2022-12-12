3c3c1021d236f7f38b393b098596696268797c0d
Bruno Albuquerque Carneiro <bruno.carneiro@wisereducacao.com>
2022-12-12 13:52:47 -0300

    feat: config theme styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,30 @@
-import { RouterContext } from 'next/dist/shared/lib/router-context'
-import GlobalStyles from '../src/styles/global'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 export const parameters = {
-  nextRouter: {
-    Provider: RouterContext.Provider
+  backgrounds: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        value: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        value: theme.colors.mainBg
+      }
+    ]
   }
 }
 
+
 export const decorators = [
-  (Story) => (
-    <>
-      <GlobalStyles />
-      <Story />
-    </>
+  (Story, ) => (
+    <ThemeProvider theme={theme}>
+      
+        <GlobalStyles />
+        <Story />
+    </ThemeProvider>
   )
 ]
--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
 module.exports = withPWA({
-  swcMinify: true,
-  experimental: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  },
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
+  // next.js config
 })
